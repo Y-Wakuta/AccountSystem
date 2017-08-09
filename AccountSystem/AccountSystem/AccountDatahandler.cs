ae9bcd09edfc5c68a835b88bd264f3bbc2a2bae2
Y-Wakuta <prsprivatestock@i.softbank.jp>
2017-08-09 22:23:53 +0900

    追加漏れのファイルをコミット

---
+++ AccountSystem/AccountSystem/AccountDatahandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountSystem {
+    public class AccountDatahandler {
+
+        BindingList<AccountsEntity> _dailyAccountList;
+        BindingList<ProductsEntity> _productsList;
+        AccountContext accountContext = new AccountContext();
+        BindingSource _pbs;
+        BindingSource _abs;
+
+        public AccountDatahandler(BindingSource pbs, BindingSource abs) {
+            _pbs = pbs;
+            _abs = abs;
+            GetAccount();
+
+            _abs.CurrentItemChanged += (s, e) => currentAccountChanged();
+            _pbs.CurrentItemChanged += (s, e) => currentProductChanged();
+        }
+
+        private void currentAccountChanged() {
+            var cur = (AccountsEntity)_abs.Current;
+            _pbs.Position = int.Parse(cur.additional_number1) - 1;
+        }
+
+        private void currentProductChanged() {
+            var cur = (ProductsEntity)_pbs.Current;
+            ((AccountsEntity)_abs.Current).additional_number1 = cur.id.ToString();
+        }
+
+        public void GetAccount() {
+            _dailyAccountList = new BindingList<AccountsEntity>(accountContext.daily_account.ToList());
+            _productsList = new BindingList<ProductsEntity>(accountContext.products.ToList());
+            _abs.DataSource = _dailyAccountList;
+            _pbs.DataSource = _productsList;
+        }
+    }
+}
---
+++ AccountSystem/AccountSystem/AccountRoutine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountSystem {
+    class AccountRoutine {
+
+        static public Func<AccountContext, ProductsEntity> GetProductById(int id) {
+            return ctx => {
+                return ctx.products
+                    .Where(p => p.id == id)
+                    .ToList()
+                    .First();
+            };
+        }
+
+        static public Func<AccountContext, AccountsEntity> GetAccountById(int id) {
+            return ctx => {
+                return ctx.daily_account
+                    .Where(a => a.id == id)
+                    .ToList()
+                    .First();
+            };
+        }
+    }
+}
